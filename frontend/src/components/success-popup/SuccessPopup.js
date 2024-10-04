fa2f703b13f9dd74a2036124887d8d37d104162e
mozlercelik <celikmehmetozler@gmail.com>
2024-10-04 23:21:31 +0300

    Bug fix on nft success dialog

--- frontend/src/components/success-popup/SuccessPopup.js
+++ frontend/src/components/success-popup/SuccessPopup.js
@@ -1,50 +1,68 @@
+import { useEffect } from "react";
 import {
-    Dialog,
-    DialogTitle,
-    DialogContent,
-    DialogActions,
-    Typography,
-    Button
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  Typography,
+  Button,
+  Box
 } from "@mui/material";
 import nftImage1 from "src/assets/nfts/level-nft.png";
 import nftImage2 from "src/assets/nfts/NFT-LevelUp.png";
 import nftImage3 from "src/assets/nfts/NFT-Premium.png";
 import Image from "next/image";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { getNftById } from "src/store/nft/nftSlice";
 
-const SuccessPopup = ({ open, handleClose, nftId, nextLabUrl }) => {
-    const { nft: nftSlice } = useSelector((state) => state);
-    useEffect(() => {
-        dispatch(getNftById(nftId));
-    }, [dispatch]);
+const SuccsesPopup = ({ open, handleClose, nftId, nextLabUrl }) => {
+  const nftSlice = useSelector((state) => state.nft);
 
-    return (
-        <Dialog open={open} onClose={handleClose}>
-            <DialogTitle>Success</DialogTitle>
-            <DialogContent>
-                <Typography variant="body1"></Typography>
-                <Typography variant="body2">
-                    Your NFT has been successfully created!
-                </Typography>
-                <Image
-                    src={nftSlice.data.data.nftId === 1 ? nftImage1 : nftSlice.data.data.nftId === 2 ? nftImage2 : nftImage3}
-                    width={220}
-                    height={220}
-                />
-            </DialogContent>
-            <DialogActions>
-                <Button
-                    onClick={() => (window.location.href = nextLabUrl)}
-                    color="dark"
-                >
-                    Go to Next Lab
-                </Button>
-                <Button onClick={handleClose} color="dark">
-                    Close
-                </Button>
-            </DialogActions>
-        </Dialog>
-    );
+  const dispatch = useDispatch();
+
+  
+  useEffect(() => {
+    dispatch(getNftById(nftId));
+  }, [dispatch]);
+
+  useEffect(() => {
+    console.log("nftSlice", nftSlice,nftId);
+  }, [nftSlice]);
+
+  return (
+    <Dialog open={open} onClose={handleClose}>
+      <DialogTitle>Success</DialogTitle>
+      <DialogContent>
+        <Typography variant="body1"></Typography>
+        <Typography variant="body2">
+          Your NFT has been successfully created!
+        </Typography>
+
+        <Box sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          mt: 2
+        }}>
+        <Image
+          src={nftId === 1 ? nftImage1 : nftId === 2 ? nftImage2 : nftImage3}
+          width={220}
+          height={220}
+          style={{
+            minWidth: "100% !important",
+            width: "100% !important",
+            height: "auto !important",
+          }}
+        />
+        </Box>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose} variant="dark">
+          Close
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
 };
 
-export default SuccessPopup;
+export default SuccsesPopup;
